20bfaa7d3ef2a4ca91022dd70729fba7c64e8dbf
Justin Hodge <justinhodge8@gmail.com>
2026-04-26 21:07:09 -0400

    formatting

--- dist/script.js
+++ dist/script.js
@@ -1,27 +1,38 @@
 const generateQR = () => {
-  const url = document.getElementById("urlInput").value.trim();
-  const qrContainer = document.getElementById("qrcode");
+    const url = document.getElementById("urlInput").value.trim();
+    const qrContainer = document.getElementById("qrcode");
 
-  qrContainer.innerHTML = "";
+    qrContainer.innerHTML = "";
 
-  if (!url) {
-    alert("Please enter a URL");
-    return;
-  }
+    if (!url) {
+        alert("Please enter a URL");
+        return;
+    }
 
-  new QRCode(qrContainer, {
-    text: url,
-    width: 200,
-    height: 200
-  });
+    new QRCode(qrContainer, {
+        text: url,
+        width: 200,
+        height: 200
+    });
 }
 
-const generateButton = 
-  document.getElementById('generate-button');
-
-if (!generateButton) {
-  console.log('generate button not found');
+const addQRLibrary = () => {
+    const script = document.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js";
+    document.body.appendChild(script);
 }
 
-generateButton
-  .addEventListener('click', generateQR)
+const init = () => {
+    document.addEventListener('DOMContentLoaded', addQRLibrary);
+
+    const generateButton =
+        document.getElementById('generate-button');
+
+    if (!generateButton) {
+        console.log('generate button not found');
+    }
+
+    generateButton
+        .addEventListener('click', generateQR)
+}
+
